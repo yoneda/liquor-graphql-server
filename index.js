d1b1bc679dc1fb5b8a5a019cece9887c9878c3ae
yoneda <nsshowdream@gmail.com>
2019-12-09 21:50:59 +0900

    [add] add some types

--- index.js
+++ index.js
@@ -2,14 +2,61 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 
 const typeDefs = `
+  enum DrinkCategory{
+    COCKTAIL
+    COFFEE
+    BEER
+    OTHER
+  }
   type Query{
     total: Int!
+    all: [Drink!]!
+  }
+  type Drink{
+    id: ID!
+    name: String!
+    url: String!
+    category: DrinkCategory
+    ingredients: String
+    instructions: String
+    date: String
   }
 `;
 
+const drinks = [
+  {
+    id: "1",
+    name: "飲み物1",
+    url: "http://google.com",
+    category: "COCKTAIL",
+    ingredients: "材料材料",
+    instructions: "手順手順",
+    date: "2019/10/10 10:00:00"
+  },
+  {
+    id: "2",
+    name: "飲み物2",
+    url: "http://google.com",
+    category: "COCKTAIL",
+    ingredients: "材料材料",
+    instructions: "手順手順",
+    date: "2019/10/10 10:00:00"
+  },
+  {
+    id: "3",
+    name: "飲み物3",
+    url: "http://google.com",
+    category: "BEER",
+    ingredients: "材料材料",
+    instructions: "手順手順",
+    date: "2019/10/10 10:00:00"
+  }
+];
+
 const resolvers = {
   Query: {
-    total: () => 10
+    total: () => drinks.length,
+    all: () => drinks
   }
 };
 
